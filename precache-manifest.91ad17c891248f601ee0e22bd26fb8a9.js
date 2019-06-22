0ab3a420568cb4ee15ceb833fd8219bf3d9a4237
JeffOgah <jeff.ogah@gmail.com>
2019-06-22 22:43:06 +0100

    Updates

---
+++ precache-manifest.91ad17c891248f601ee0e22bd26fb8a9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1827e97bb0b49f85bcbde5a9a309304",
+    "url": "/matching-game/index.html"
+  },
+  {
+    "revision": "d86ed341fa7856978193",
+    "url": "/matching-game/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "c89d8d2b98c9e8586e6d",
+    "url": "/matching-game/static/css/main.3bb8df2f.chunk.css"
+  },
+  {
+    "revision": "d86ed341fa7856978193",
+    "url": "/matching-game/static/js/2.ed1bc2ba.chunk.js"
+  },
+  {
+    "revision": "c89d8d2b98c9e8586e6d",
+    "url": "/matching-game/static/js/main.a82f4609.chunk.js"
+  },
+  {
+    "revision": "e43d7fc81bcf6d217bd3",
+    "url": "/matching-game/static/js/runtime~main.c314e423.js"
+  },
+  {
+    "revision": "e4ef38b67fed768b125cf0a8d0e8f95b",
+    "url": "/matching-game/static/media/camera.e4ef38b6.png"
+  },
+  {
+    "revision": "8e8b78658c42c852b46cc9162ef92588",
+    "url": "/matching-game/static/media/idea.8e8b7865.png"
+  }
+]);
